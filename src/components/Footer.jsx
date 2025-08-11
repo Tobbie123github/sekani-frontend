5337011f20ad7cc0cc7a4add3efa8afd754ba754
Tobbie123github <tobbie41973@gmail.com>
2025-08-11 14:20:14 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ const Footer = () => {
   return (
     <footer>
         <div className="container mx-auto py-4 mt-[3rem] text-center">
-            <p className="text-primary dark:text-gray-400">Made with <span>❤️</span> by <a href='https://marshall-beta.vercel.app/' className="text-[#B05015] font-semibold">Marshall</a></p>
+            <p className="text-primary dark:text-gray-400">Made with <span>❤️</span> by <a href='https://marshall-beta.vercel.app/' className="text-[#B05015] font-semibold" target="_blank">Marshall</a></p>
         </div>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
